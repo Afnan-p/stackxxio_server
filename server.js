b4e20f78dc5f4ef2fcc37de17be24d7890f6914b
Afnan-p <afnanalanvar@gmail.com>
2026-05-04 20:34:52 +0530

    fix cors properly

--- server.js
+++ server.js
@@ -16,7 +16,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://stacxxio.vercel.app" // 👈 your frontend URL
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 import projectRoutes from './routes/projects.js';
